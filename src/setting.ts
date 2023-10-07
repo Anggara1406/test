85fe0a4ce806a98ec217b47ac7289881b70af432
Anggara1406 <118477342+Anggara1406@users.noreply.github.com>
2023-10-08 02:01:12 +0700

    Create setting.ts

---
+++ src/setting.ts
@@ -0,0 +1,4 @@
+export const TokenAddress = '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2';
+export const PresaleAddress = '0xc2F44Bc508b6b50047a2F3AfB1984eD105070bE1';
+export const CheckAndSendAddress = '0xC4595E3966e0Ce6E3c46854647611940A09448d3';
+export const TokenBalance = '292424432826802010';
